e3759ecb190a872ff0efd27ac718209ecb074405
Praise Jacob <iamfelixkyun@gmail.com>
2025-03-28 21:04:32 +0530

    complete 2.11 (yes 2.10 also included)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import { CreationForm } from "./CreationForm";
 import { PersonRenderer } from "./PersonsRenderer";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
   const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/persons")
+      // .then((res) => res.json()) // only for fetch
+      .then(raw => raw.data)
+      .then((data) => setPersons(data));
+  }, []);
 
   return (
     <>
