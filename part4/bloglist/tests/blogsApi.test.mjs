57742e9937e5c1546748c9110ab2d268f32506e3
Praise Jacob <iamfelixkyun@gmail.com>
2025-04-06 00:57:23 +0530

    complete 4.9

--- part4/bloglist/tests/blogsApi.test.mjs
+++ part4/bloglist/tests/blogsApi.test.mjs
@@ -1,10 +1,10 @@
 import {
-  test,
-  after,
-  describe,
-  before,
-  beforeEach,
-  afterEach,
+	test,
+	after,
+	describe,
+	before,
+	beforeEach,
+	afterEach,
 } from "node:test";
 import assert from "node:assert";
 import supertest from "supertest";
@@ -15,35 +15,47 @@ import rawBlogs from "./blogs.mjs";
 
 const api = supertest(app);
 const blogs = rawBlogs.map((blog) => {
-  delete blog._id;
-  delete blog.__v;
+	blog.id = blog._id;
+	delete blog._id;
+	delete blog.__v;
 
-  return blog;
+	return blog;
 });
 
 describe("Blogs Api Tests", () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({});
-    // as Blog.create doesn't maintain order
-    for (const blog of blogs) await Blog.create(blog);
-  });
+	beforeEach(async () => {
+		await Blog.deleteMany({});
+		// as Blog.create doesn't maintain order
+		for (const blog of blogs) {
+			const { id, ...withoutId } = blog;
+			await Blog.create({ _id: id, ...withoutId });
+		}
+	});
 
-  test("GET /blogs returns all blogs in JSON format", async () => {
-    const response = await api
-      .get("/api/blogs")
-      .expect(200)
-      .expect("Content-Type", /application\/json/);
+	test("GET /blogs returns all blogs in JSON format", async () => {
+		const response = await api
+			.get("/api/blogs")
+			.expect(200)
+			.expect("Content-Type", /application\/json/);
 
-    const blogsInResponse = response.body.map((blog) => {
-      delete blog.id;
-      return blog;
-    });
+		const blogsInResponse = response.body.map((blog) => {
+			return blog;
+		});
 
-    assert.strictEqual(blogs.length, blogsInResponse.length);
-    assert.deepStrictEqual(blogs, blogsInResponse);
-  });
+		assert.strictEqual(blogs.length, blogsInResponse.length);
+		assert.deepStrictEqual(blogs, blogsInResponse);
+	});
 
-  after(async () => {
-    await mongoose.connection.close();
-  });
+	test("all the blogs have their _id property replaced with id", async () => {
+		const response = await api.get("/api/blogs").expect(200);
+		const blogsInResponse = response.body;
+
+		assert.ok(
+			blogsInResponse.every((blog, index) => blogs[index].id === blog.id)
+		);
+	});
+
+	after(async () => {
+		await mongoose.connection.close();
+	});
 });
