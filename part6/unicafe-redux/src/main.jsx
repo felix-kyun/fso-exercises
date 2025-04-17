6efd22011ec97c06b01260697cd7e82b0b40311b
Praise Jacob <iamfelixkyun@gmail.com>
2025-04-17 11:32:44 +0530

    complete till 6.2

---
+++ part6/unicafe-redux/src/main.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { createStore } from "redux";
+import reducer from "./reducer";
+
+const store = createStore(reducer);
+
+const App = () => {
+  const actionDispatcher = (type) => store.dispatch({ type });
+  const good = () => actionDispatcher("GOOD");
+  const ok = () => actionDispatcher("OK");
+  const bad = () => actionDispatcher("BAD");
+  const reset = () => actionDispatcher("ZERO");
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>ok</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
