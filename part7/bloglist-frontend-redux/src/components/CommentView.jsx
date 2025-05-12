92941f7ce0fd154582d097927dc1cf435bb111d4
Praise Jacob <iamfelixkyun@gmail.com>
2025-05-13 01:27:02 +0530

    complete 7.19

--- part7/bloglist-frontend-redux/src/components/CommentView.jsx
+++ part7/bloglist-frontend-redux/src/components/CommentView.jsx
@@ -5,6 +5,7 @@ import { useMatch } from "react-router";
 export const CommentView = () => {
   const match = useMatch("/blogs/:id");
   const [comments, setComments] = useState([]);
+  const [input, setInput] = useState("");
   const id = match?.params.id;
 
   useEffect(() => {
@@ -20,11 +21,31 @@ export const CommentView = () => {
     fetchComments(id);
   }, [id]);
 
+  async function handleCreation(ev) {
+    const res = await fetch(`http://localhost:3000/api/blogs/${id}/comments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ comment: input }),
+    });
+
+    const createdComment = await res.json();
+    setComments([...comments, createdComment]);
+    setInput("");
+  }
+
   if (!match) return null;
 
   return (
     <div>
       <h3>Comments</h3>
+      <input
+        type="text"
+        value={input}
+        onChange={(ev) => setInput(ev.target.value)}
+      />
+      <button onClick={handleCreation}>Comment</button>
       <ul>
         {comments.map(({ comment, id }) => (
           <li key={id}>{comment}</li>
