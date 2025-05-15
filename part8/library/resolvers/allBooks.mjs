df96c2acda31d3c5deacec38a6dd6c958cd38b0d
Praise Jacob <iamfelixkyun@gmail.com>
2025-05-16 02:34:28 +0530

    complete 8.4 and 8.5

--- part8/library/resolvers/allBooks.mjs
+++ part8/library/resolvers/allBooks.mjs
@@ -1,5 +1,15 @@
 import { books } from "../books.mjs";
 
-export function allBooks(root, args) {
-  return books;
+export function allBooks(root, { author, genre }) {
+  let queryBooks = books;
+
+  if (author) {
+    queryBooks = queryBooks.filter((b) => b.author === author);
+  }
+
+  if (genre) {
+    queryBooks = queryBooks.filter((b) => b.genres.includes(genre));
+  }
+
+  return queryBooks;
 }
