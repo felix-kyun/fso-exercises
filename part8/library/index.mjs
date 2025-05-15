7d0a1e02b7e054fc1c15c7ccdefba267d575c1e2
Praise Jacob <iamfelixkyun@gmail.com>
2025-05-16 01:41:19 +0530

    complete 8.1

---
+++ part8/library/index.mjs
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { books } from "./books.mjs";
+import { authors } from "./authors.mjs";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeTypeDefs } from "@graphql-tools/merge";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const typeArray = loadFilesSync(path.join(__dirname, "./*.gql"));
+const typeDefs = mergeTypeDefs(typeArray);
+
+const resolvers = {
+  Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: {
+    port: 4000,
+  },
+});
+
+console.log(`Server ready at ${url}`);
