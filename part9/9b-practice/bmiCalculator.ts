6124be50dfefd3ba50ae307273541cc81a097a77
Praise Jacob <iamfelixkyun@gmail.com>
2025-07-04 17:53:07 +0530

    complete 9.5

--- part9/9b-practice/bmiCalculator.ts
+++ part9/9b-practice/bmiCalculator.ts
@@ -6,7 +6,7 @@ const bmi = (height: number, weight: number): number =>
 
 const toMetre = (cm: number): number => cm / 100;
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
     const calculatedBmi = bmi(toMetre(height), weight);
 
     if (calculatedBmi < 16.0) return "Underweright (Severe thinness)";
@@ -19,17 +19,18 @@ const calculateBmi = (height: number, weight: number): string => {
     else return "Obese (Class III)";
 };
 
-try {
-    if (argv.length != 4) {
-        console.error("it only accepts two values as input");
-        exit(1);
-    }
+if (require.main === module)
+    try {
+        if (argv.length != 4) {
+            console.error("it only accepts two values as input");
+            exit(1);
+        }
 
-    const height = toNumber(argv[2]);
-    const weight = toNumber(argv[3]);
-    console.log(calculateBmi(height, weight));
-} catch (error: any) {
-    if (error instanceof Error) {
-        console.error(error.message);
+        const height = toNumber(argv[2]);
+        const weight = toNumber(argv[3]);
+        console.log(calculateBmi(height, weight));
+    } catch (error: any) {
+        if (error instanceof Error) {
+            console.error(error.message);
+        }
     }
-}
--- part9/9b-practice/index.ts
+++ part9/9b-practice/index.ts
@@ -1,7 +1,9 @@
-import express from "express";
+import express, { Request, Response, Express } from "express";
+import { toNumber } from "./utils";
+import { calculateBmi } from "./bmiCalculator";
 
-const PORT = 3000;
-const app = express();
+const PORT: number = 3000;
+const app: Express = express();
 
 app.get("/ping", async (_req, res) => {
     res.send("pong");
@@ -9,6 +11,43 @@ app.get("/ping", async (_req, res) => {
 
 app.get("/hello", async (_req, res) => {
     res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", async (req: Request, res: Response) => {
+    const { height, weight } = req.query as {
+        height?: string;
+        weight?: string;
+    };
+
+    if (!height || !weight) {
+        res.status(400).json({
+            error: "malformatted parameters",
+        });
+        return;
+    }
+
+    try {
+        const heightInCm: number = toNumber(height);
+        const weightInKg: number = toNumber(weight);
+
+        res.json({
+            height: heightInCm,
+            weight: weightInKg,
+            bmi: calculateBmi(heightInCm, weightInKg),
+        });
+        return;
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(400).json({
+                error: error.message,
+            });
+            return;
+        }
+    }
+
+    res.status(500).json({
+        error: "something went wrong",
+    });
 });
 
 app.listen(PORT, () => {
