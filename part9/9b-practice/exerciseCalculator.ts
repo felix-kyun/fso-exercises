c3dca60eccfabd61da2cf4947d79dc35a7505464
Praise Jacob <iamfelixkyun@gmail.com>
2025-07-06 13:42:52 +0530

    complete 9.7

--- part9/9b-practice/exerciseCalculator.ts
+++ part9/9b-practice/exerciseCalculator.ts
@@ -11,7 +11,10 @@ interface ExerciseStats {
     average: number;
 }
 
-const calculateExercises = (data: number[], target: number): ExerciseStats => {
+export const calculateExercises = (
+    data: number[],
+    target: number
+): ExerciseStats => {
     const average = data.reduce((prev, cur) => prev + cur, 0) / data.length;
     const trainingDays = data.filter((n) => n != 0).length;
     const rating = average >= target ? 3 : data.every((n) => n != 0) ? 2 : 1;
@@ -33,19 +36,20 @@ const calculateExercises = (data: number[], target: number): ExerciseStats => {
     };
 };
 
-try {
-    if (argv.length < 4) {
-        console.error("two or more values required");
-        exit(1);
-    }
+if (require.main === module)
+    try {
+        if (argv.length < 4) {
+            console.error("two or more values required");
+            exit(1);
+        }
 
-    const [, , target, ...daily] = argv;
-    const parsedDaily = daily.map(toNumber);
-    const parsedTarget = toNumber(target);
+        const [, , target, ...daily] = argv;
+        const parsedDaily = daily.map(toNumber);
+        const parsedTarget = toNumber(target);
 
-    console.log(calculateExercises(parsedDaily, parsedTarget));
-} catch (error: any) {
-    if (error instanceof Error) {
-        console.error(error.message);
+        console.log(calculateExercises(parsedDaily, parsedTarget));
+    } catch (error: any) {
+        if (error instanceof Error) {
+            console.error(error.message);
+        }
     }
-}
--- part9/9b-practice/index.ts
+++ part9/9b-practice/index.ts
@@ -1,19 +1,21 @@
 import express, { Request, Response, Express } from "express";
 import { toNumber } from "./utils";
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
 
 const PORT: number = 3000;
 const app: Express = express();
+app.use(express.json());
 
-app.get("/ping", async (_req, res) => {
+app.get("/ping", (_req, res) => {
     res.send("pong");
 });
 
-app.get("/hello", async (_req, res) => {
+app.get("/hello", (_req, res) => {
     res.send("Hello Full Stack!");
 });
 
-app.get("/bmi", async (req: Request, res: Response) => {
+app.get("/bmi", (req: Request, res: Response) => {
     const { height, weight } = req.query as {
         height?: string;
         weight?: string;
@@ -48,6 +50,39 @@ app.get("/bmi", async (req: Request, res: Response) => {
     res.status(500).json({
         error: "something went wrong",
     });
+});
+
+app.post("/exercises", (req: Request, res: Response) => {
+    const { daily_exercises, target } = req.body as {
+        daily_exercises?: number[];
+        target?: number;
+    };
+
+    if (!daily_exercises || !target) {
+        res.status(400).json({
+            error: "parameters missing",
+        });
+        return;
+    }
+
+    // data type check
+    try {
+        const dailyData = daily_exercises.map(toNumber);
+        const targetData = toNumber(target);
+
+        res.status(200).json(calculateExercises(dailyData, targetData));
+    } catch (error: unknown) {
+        if (error instanceof Error)
+            res.status(400).json({
+                error: "malformatted parameters",
+            });
+        else
+            res.status(500).json({
+                error: "something went wrong",
+            });
+    }
+
+    return;
 });
 
 app.listen(PORT, () => {
